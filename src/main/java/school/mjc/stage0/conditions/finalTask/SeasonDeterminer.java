a2079396a2808d662e913225a6fe9cf60c296e30
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 00:19:07 +0600

    fix2mod4taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
-        if(monthNumber < 0 || monthNumber > 12) System.out.println("Wrong month number");
+        if(monthNumber < 1 || monthNumber > 12) System.out.println("Wrong month number");
         else if(monthNumber == 12 || monthNumber <= 2) {
             System.out.println("Winter");
         }else if(monthNumber <= 5) {
